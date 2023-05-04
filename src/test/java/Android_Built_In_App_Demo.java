1d667526407744c6d5e04b0adacae4d392b60676
emredursun <info.emredursun@gmail.com>
2023-05-04 15:20:38 +0200

    add Android_Built_In_App_Demo class to demostrate how to write a simple Test Case and verify the test result with an  Assertion

--- src/test/java/Android_Built_In_App_Demo.java
+++ src/test/java/Android_Built_In_App_Demo.java
@@ -1,9 +1,9 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -11,7 +11,7 @@ import org.testng.annotations.Test;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class AndroidTest01 {
+public class Android_Built_In_App_Demo {
 
     AppiumDriver driver;
 
@@ -22,16 +22,21 @@ public class AndroidTest01 {
         caps.setCapability("automationName", "UiAutomator2");
         caps.setCapability("platformVersion", "9.0");
         caps.setCapability("deviceName", "Android Emulator");
-        caps.setCapability("app", System.getProperty("user.dir")+"\\apps\\ApiDemos.apk");
+//        caps.setCapability("app", System.getProperty("user.dir")+"\\apps\\ApiDemos.apk");
+        caps.setCapability("appPackage","com.android.calculator2");
+        caps.setCapability("appActivity",".Calculator");
         driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"),caps);
     }
 
     @Test
-    public void click_App_Button(){
-        // code
-        driver.findElementByAccessibilityId("App").click();
+    public void click_test(){
+        driver.findElement(By.id("digit_1")).click();
+        driver.findElement(By.id("op_add")).click();
+        driver.findElement(By.id("digit_3")).click();
+        driver.findElement(By.id("eq")).click();
+        String result = driver.findElement(By.id("result")).getText();
+        Assert.assertEquals(result, "4");
     }
-
     @AfterTest
     public void tearDown() {
         if(null != driver){
